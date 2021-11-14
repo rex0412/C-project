dd3b5b5f451c3552f9e332372455b291f63d4cc5
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2021-11-14 17:58:16 +0900

    8월 과제 5번

---
+++ 8월 과제 5번.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ main( ) {
+int n[6]={0, 3, 1, 5, 1, 4};
+int a, k;
+ for(a=1; a <= 5; a++){
+      k = n[a];
+   n[k] = n[a];
+ }
+ for(a = 1; a <= 5; a++)
+   printf("%d ", n[a]);
+}
