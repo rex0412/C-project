b1d75638ac11ad114cbbfa47e301681f3601cabc
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2021-12-24 22:29:04 +0900

    11월 과제 1-4번

---
+++ 11월 과제 1-4번.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main( ){
+int a, b, s = 0;
+int n[4][4]={{1,2,3,4},{4,5,6,7},{5, 9, 8,10}};
+for(a=0; a<=3; a++)
+  for(b=0; b<=3; b++)
+    if (a >= b)
+      s = s + n[a][b];
+printf("%d", s);
+}
